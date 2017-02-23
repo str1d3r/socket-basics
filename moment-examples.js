b2135cb08f593db23bc1365ebcd8e4bb2f0af437
Jordan Ehrlich <jehrlich@harriscomputer.com>
2017-02-23 13:35:24 -0600

    Add moment example and file

---
+++ moment-examples.js
@@ -0,0 +1,15 @@
+var moment=require('moment');
+var now=moment();
+
+console.log(now.format());
+console.log(now.format('X'));
+console.log(now.valueOf());
+
+var timestamp=1487877751086;
+var timestampMoment=moment.utc(timestamp);
+console.log(timestampMoment.local().format('h:mm a'));
+
+// now.subtract(1,'year');
+//
+// console.log(now.format());
+// console.log(now.format('MMM Do YYYY, h:mma'));
