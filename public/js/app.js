99fdff71e14b7d7ff75690fcf667e54434fd4458
Jordan Ehrlich <jehrlich@harriscomputer.com>
2017-02-23 16:39:28 -0600

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,33 @@
 var socket = io();
-
+var name = getQueryVariable('name')||'Anonymous';
+var room = getQueryVariable('room');
 socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
+	console.log(name + ' joined room ' + room);
+
+
 });
 
 socket.on('message', function(message) {
+	var $message = jQuery('.messages');
 	console.log('New message:');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p><strong>'+moment.utc(message.timestamp).local().format('h:mm a') + ':</strong> ' + message.text + '</p>');
-});
+	$message.append('<p><strong>' + message.name + ' ' + moment.utc(message.timestamp).local().format('h:mm a') + '</strong></p>');
+	$message.append('<p>' + message.text + '</p>');
 
+});
 // Handles submitting of new message
 var $form = jQuery('#message-form');
 
 $form.on('submit', function(event) {
 	event.preventDefault();
 
-	var $message=$form.find('input[name=message]');
+	var $message = $form.find('input[name=message]');
 
 	socket.emit('message', {
-		text: $message.val()
-
+		text: $message.val(),
+		name: name
 	});
 	$message.val('').focus();
 });
--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-var PORT=process.env.PORT||3000;
-var express=require('express');
-var app=express();
-var http=require('http').Server(app);
-var io=require('socket.io')(http);
-var moment=require('moment');
+var PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var moment = require('moment');
 
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
-io.on('connection',function(socket){
-  console.log('User connected via socket.io!');
+io.on('connection', function(socket) {
+	console.log('User connected via socket.io!');
 
-  socket.on('message',function(message){
-    var timestampMoment=moment().valueOf();
-    message.timestamp =timestampMoment;
-    console.log('Message received: '+message.timestamp + ' '+message.text);
-    io.emit('message',message);
-  });
+	socket.on('message', function(message) {
+		var timestampMoment = moment().valueOf();
+		message.timestamp = timestampMoment;
+		console.log('Message received: ' + message.timestamp + ' ' + message.text);
+		io.emit('message', message);
+	});
 
-  socket.emit('message',{
-    text: 'Welcome to the chat application!',
-    timestamp: moment().valueOf()
-  });
+	socket.emit('message', {
+		name: 'System',
+		text: 'Welcome to the chat application!',
+		timestamp: moment().valueOf()
+	});
 });
 
-http.listen(PORT,function(){
-  console.log('Server started!');
+http.listen(PORT, function() {
+	console.log('Server started!');
 })
