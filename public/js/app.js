0beb61167c22b882ca1764755a4ee2c4a8906f99
Jordan Ehrlich <jehrlich@harriscomputer.com>
2017-02-24 13:22:28 -0600

    Add changes for chat app

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,27 @@
 var socket = io();
-var name = getQueryVariable('name')||'Anonymous';
+var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
 socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
 	console.log(name + ' joined room ' + room);
 	jQuery('.room-title').text(room);
-	socket.emit('joinRoom',{
-		name:name,
-		room:room
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
 	});
 
 });
 
 socket.on('message', function(message) {
-	var $message = jQuery('.messages');
+	var $messages = jQuery('.messages');
+	var $message = jQuery('<li class="list-group-item"></li>')
+
 	console.log('New message:');
 	console.log(message.text);
 
 	$message.append('<p><strong>' + message.name + ' ' + moment.utc(message.timestamp).local().format('h:mm a') + '</strong></p>');
 	$message.append('<p>' + message.text + '</p>');
-
+	$messages.append($message);
 });
 // Handles submitting of new message
 var $form = jQuery('#message-form');
